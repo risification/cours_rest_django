9951e60a9af81ed2b52367698c90f6e4f8c022a0
risification <sultangaziev01@bk.ru>
2021-03-26 00:06:12 +0600

    urls add profile/, table/, table/<int:table_id>/, userprofile/

---
+++ accounts/urls_accounts.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('profile/', RestaurantProfileView.as_view()),
+    path('table/', TableView.as_view()),
+    path('table/<int:table_id>/', TableDetailView.as_view()),
+    path('userprofile/', UserProfileView.as_view())
+]
